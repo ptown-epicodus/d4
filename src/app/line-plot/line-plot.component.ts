8ca7fdbac6963509cc72867eb0afdfb112da3615
Patrick McGreevy <patrick7490@icloud.com>
2017-04-13 11:14:50 -0700

    Refactor LinePlot component, add legend

--- src/app/line-plot/line-plot.component.ts
+++ src/app/line-plot/line-plot.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { D3Service, D3, Selection, ScaleLinear, Axis } from 'd3-ng2-service';
 
-import { SEASONS } from '../bobross_data';
+import { SEASONS, PICTURE_FEATURES } from '../bobross_data';
 
 @Component({
   selector: 'app-line-plot',
@@ -11,12 +11,12 @@ import { SEASONS } from '../bobross_data';
 export class LinePlotComponent implements OnInit {
   private d3: D3; // <-- Define the private member which will hold the d3 reference
 
-  private brData;
-  private lineData;
-
+  private lineData: any[][][];
   width: number; // These are gobal values used to scale the component
   height: number; // These are gobal values used to scale the component
   selection: any;
+  private pictureFeatures: string[] = PICTURE_FEATURES;
+  private featuresShowing: boolean[] =[];
 
 
   constructor(d3Service: D3Service) { // <-- pass the D3 Service into the constructor
@@ -25,8 +25,11 @@ export class LinePlotComponent implements OnInit {
 
   ngOnInit() {
     this.parseData();
+    for (var i = 0; i < this.pictureFeatures.length; i++) {
+      this.featuresShowing[i] = (i < 5); // start with only first 5 features showing
+    }
     // this creates an svg element in the template and saves the reference for future use by display functions
-    var w = 500,
+    var w = 1000,
         h = 500;
     var margin = {top: 20, right: 20, bottom: 30, left: 50};
     this.width = w - margin.left - margin.right;
@@ -37,6 +40,7 @@ export class LinePlotComponent implements OnInit {
                             .attr("width", this.width + margin.left + margin.right)
                             .attr("height", this.height + margin.top + margin.bottom)
                             .append("g")
+                            .attr("id", "global-g")
                             .attr("transform",
                                   "translate(" + margin.left + "," + margin.top + ")");
 
@@ -50,19 +54,24 @@ export class LinePlotComponent implements OnInit {
   }
 
   drawLine(width, height) {
+    let d3 = this.d3;
+    let lineData =
+    this.lineData.filter((el, index) => {
+      return this.featuresShowing[index];
+    });
     // this is the key portion that lets this function correctly draw the element as the data is updated.  This takes our selection property then selects the elements within it that we will act on ('rect' in this case), and joins the data.  The reference to this data join is then stored in the join variable.
-    let join = this.selection.selectAll('path').data(this.lineData);
+    let join = this.selection.selectAll('path.line').data(lineData);
 
-    let xScale = this.d3.scaleLinear()
-              .domain([0, this.d3.max(this.lineData[0], function(d: number[]) { return d[0]; })])
+    let xScale = d3.scaleLinear()
+              .domain([0, d3.max(this.lineData, function(d: any[][]) { return d3.max(d, function(e: number[]) { return e[0]; }); })])
               .range([0, width]);
 
-    let yScale = this.d3.scaleLinear()
-              .domain([0, this.d3.max(this.lineData[0], function(d: number[]) { return d[1]; })])
+    let yScale = d3.scaleLinear()
+              .domain([0, d3.max(this.lineData, function(d: any[][]) { return d3.max(d, function(e: number[]) { return e[1]; }); })])
               .range([height, 0]);
 
 
-    let drawLine = this.d3.line()
+    let drawLine = d3.line()
                           .x(function(d) {return xScale(d[0]); })
                           .y(function(d) {return yScale(d[1]); });
 
@@ -74,69 +83,54 @@ export class LinePlotComponent implements OnInit {
       .merge(join) // puts the update and enter sections of the join together so that all following methods will act on all elements.
       .transition() // Sets the default transition as display changes, and then modifies attributes of all elements in the join.
       .attr('d', drawLine)
-      .attr('class', 'line')
-      .attr('stroke', function(d, i) {
-        return "hsl(" + Math.random() * 360 + ",100%,50%)";
+      .attr('class', function(d, i) {
+        return 'line line-' + d.feature;
       });
+
+    this.selection.selectAll(".axis")
+                  .remove();
 
     // Add the X Axis
     this.selection.append("g")
                   .attr("transform", "translate(0," + this.height + ")")
-                  .call(this.d3.axisBottom(xScale));
+                  .attr("class", "axis")
+                  .call(d3.axisBottom(xScale));
 
     // Add the Y Axis
     this.selection.append("g")
-                  .call(this.d3.axisLeft(yScale));
+                  .attr("class", "axis")
+                  .call(d3.axisLeft(yScale));
 
   }
 
   parseData() {
-    let pictureFeatures:string[] = [
-      'bushes',
-      'cabin',
-      'cactus',
-      'circle_frame',
-      'cirrus',
-      'cliff',
-      'clouds',
-      'conifer',
-      'cumulus',
-      'decidious',
-      'grass',
-      'lake',
-      'mountain',
-      'mountains',
-      'river',
-      'snow',
-      'snowy_mountain',
-      'tree',
-      'trees',
-      'waterfall',
-      'waves',
-      'winter'
-    ];
-
-    var bushData = [];
     var data = {};
     var lineData = [];
-    for(var i = 0; i < pictureFeatures.length; i++) {
-      data[pictureFeatures[i]] = [];
+    for(var i = 0; i < PICTURE_FEATURES.length; i++) {
+      data[PICTURE_FEATURES[i]] = [];
     }
     var numSeasons:number = 30;
 
     for(var i = 0; i < numSeasons; i++) {
       var seasonName:string = 'season_' + (i+1);
-      for(var j = 0; j < pictureFeatures.length; j++) {
-        var propertyName:string = pictureFeatures[j];
+      for(var j = 0; j < PICTURE_FEATURES.length; j++) {
+        var propertyName:string = PICTURE_FEATURES[j];
         data[propertyName][i] = [i+1, SEASONS[seasonName][propertyName]];
       }
     }
 
-    for(var i = 0; i < 5; i++) {
-      lineData[i] = data[pictureFeatures[i]];
+    var numFeaturesToPlot: number = PICTURE_FEATURES.length;
+    for(var i = 0; i < numFeaturesToPlot; i++) {
+      lineData[i] = data[PICTURE_FEATURES[i]];
     }
-    this.brData = data;
+    for(var i = 0; i < numFeaturesToPlot; i++) {
+      lineData[i]['feature'] = PICTURE_FEATURES[i];
+    }
     this.lineData = lineData;
+  }
+
+  toggleFeature(index: number) {
+    this.featuresShowing[index] = ! this.featuresShowing[index];
   }
 
 
