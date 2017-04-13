63efd1ba9d9e5a091937c02b0b3888d9b3ae25a3
Patrick McGreevy <patrick7490@icloud.com>
2017-04-13 08:57:56 -0700

    Switch line plot to use data source

--- src/app/line-plot/line-plot.component.ts
+++ src/app/line-plot/line-plot.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { D3Service, D3, Selection, ScaleLinear, Axis } from 'd3-ng2-service';
 
+import { SEASONS } from '../bobross_data';
+
 @Component({
   selector: 'app-line-plot',
   templateUrl: './line-plot.component.html',
@@ -9,20 +11,9 @@ import { D3Service, D3, Selection, ScaleLinear, Axis } from 'd3-ng2-service';
 export class LinePlotComponent implements OnInit {
   private d3: D3; // <-- Define the private member which will hold the d3 reference
 
-  data: number[][] = [
-      [1,2],
-      [3,4],
-      [5,6],
-      [7,8],
-      [9,10]
-  ];
-  data2: number[][] = [
-      [1,2],
-      [2,4],
-      [3,6],
-      [4,8],
-      [5,10]
-  ];
+  private brData;
+  private lineData;
+
   width: number; // These are gobal values used to scale the component
   height: number; // These are gobal values used to scale the component
   selection: any;
@@ -33,6 +24,7 @@ export class LinePlotComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.parseData();
     // this creates an svg element in the template and saves the reference for future use by display functions
     var w = 500,
         h = 500;
@@ -59,14 +51,14 @@ export class LinePlotComponent implements OnInit {
 
   drawLine(width, height) {
     // this is the key portion that lets this function correctly draw the element as the data is updated.  This takes our selection property then selects the elements within it that we will act on ('rect' in this case), and joins the data.  The reference to this data join is then stored in the join variable.
-    let join = this.selection.selectAll('path').data([this.data, this.data2]);
+    let join = this.selection.selectAll('path').data(this.lineData);
 
     let xScale = this.d3.scaleLinear()
-              .domain([0, this.d3.max(this.data, function(d: number[]) { return d[0]; })])
+              .domain([0, this.d3.max(this.lineData[0], function(d: number[]) { return d[0]; })])
               .range([0, width]);
 
     let yScale = this.d3.scaleLinear()
-              .domain([0, this.d3.max(this.data, function(d: number[]) { return d[1]; })])
+              .domain([0, this.d3.max(this.lineData[0], function(d: number[]) { return d[1]; })])
               .range([height, 0]);
 
 
@@ -83,6 +75,9 @@ export class LinePlotComponent implements OnInit {
       .transition() // Sets the default transition as display changes, and then modifies attributes of all elements in the join.
       .attr('d', drawLine)
       .attr('class', 'line')
+      .attr('stroke', function(d, i) {
+        return "hsl(" + Math.random() * 360 + ",100%,50%)";
+      });
 
     // Add the X Axis
     this.selection.append("g")
@@ -93,6 +88,55 @@ export class LinePlotComponent implements OnInit {
     this.selection.append("g")
                   .call(this.d3.axisLeft(yScale));
 
+  }
+
+  parseData() {
+    let pictureFeatures:string[] = [
+      'bushes',
+      'cabin',
+      'cactus',
+      'circle_frame',
+      'cirrus',
+      'cliff',
+      'clouds',
+      'conifer',
+      'cumulus',
+      'decidious',
+      'grass',
+      'lake',
+      'mountain',
+      'mountains',
+      'river',
+      'snow',
+      'snowy_mountain',
+      'tree',
+      'trees',
+      'waterfall',
+      'waves',
+      'winter'
+    ];
+
+    var bushData = [];
+    var data = {};
+    var lineData = [];
+    for(var i = 0; i < pictureFeatures.length; i++) {
+      data[pictureFeatures[i]] = [];
+    }
+    var numSeasons:number = 30;
+
+    for(var i = 0; i < numSeasons; i++) {
+      var seasonName:string = 'season_' + (i+1);
+      for(var j = 0; j < pictureFeatures.length; j++) {
+        var propertyName:string = pictureFeatures[j];
+        data[propertyName][i] = [i+1, SEASONS[seasonName][propertyName]];
+      }
+    }
+
+    for(var i = 0; i < 5; i++) {
+      lineData[i] = data[pictureFeatures[i]];
+    }
+    this.brData = data;
+    this.lineData = lineData;
   }
 
 
